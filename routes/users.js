3f5318e30743ce52b2bcd63c2a71f898103c71d6
Faisal282 <faisallionel@gmail.com>
2019-02-25 22:35:14 +0700

    commit kedua

--- routes/users.js
+++ routes/users.js
@@ -52,7 +52,7 @@ app.post('/add', function (req, res, next) {
 			email: req.sanitize('email').escape().trim(),
 			no: req.sanitize('no').escape().trim(),
 		}
-		req.db.collection('coba').insert(user, function (err, result) {
+		req.db.collection('user').insert(user, function (err, result) {
 			if (err) {
 				req.flash('error', err)
 
@@ -90,7 +90,88 @@ app.post('/add', function (req, res, next) {
 
 app.get('/edit/:id', function (req, res, next) {
 	var o_id = new ObjectId(req.params.id)
-	req.db.collection('coba')
+	req.db.collection('user').find({"_id":o_id}).toArray(function (err, result) {
+		if (err) return console.log(err)
+
+		if (!result) {
+			req.flash('error', 'User not found with id = ' + req.params.id)
+			res.redirect('/users')
+		} else {
+			res.render('user/edit', {
+				title: "EDIT DATA",
+				id: result[0]._id,
+				nama: result[0].nama,
+				jurusan: result[0].jurusan,
+				email: result[0].email,
+				no: result[0].no,
+			})
+		}
+	})
+})
+
+app.put('/edit/:id', function (req, res, next) {
+	req.assert('nama', 'Nama is required').notEmpty()
+	req.assert('jurusan', 'Jurusan is required').notEmpty()
+	req.assert('email', 'Email is required').notEmpty()
+	req.assert('no', 'No is required').notEmpty()
+
+	var errors = req.validationErrors()
+
+	if (!errors) {
+		var user = {
+			nama: req.sanitize('nama').escape().trim(),
+			jurusan: req.sanitize('jurusan').escape().trim(),
+			email: req.sanitize('email').escape().trim(),
+			no: req.sanitize('no').escape().trim(),
+		}
+
+		var o_id = new ObjectId(req.params.id)
+		req.db.collection('user').update({"id":o_id}, user, function (err, result) {
+			if (err) {
+				req.flash('error', err)
+
+				res.render('user/edit', {
+					title: 'EDIT DATA',
+					id:req.params.id,
+					nama:req.body.nama,
+					jurusan:req.body.jurusan,
+					email:req.body.email,
+					no:req.body.no,
+				})
+			} else {
+				req.flash('Berhasil', 'Data berhasil diupdate')
+				res.redirect('/tampil')
+			}
+		})
+	} else {
+		var error_msg = ''
+		errors.forEach(function (error) {
+			error_msg += error.msg + '<br>'
+		})
+		req.flash('error', error_msg)
+
+		res.render('user/edit', {
+			title: "EDIT DATA",
+			id: req.params.id,
+			nama: req.body.nama,
+			jurusan: req.body.jurusan,
+			email: req.body.email,
+			no: req.body.no
+		})
+	}
+})
+
+app.delete('/delete/:id', function (req, res, next) {
+	var o_id = new ObjectId(req.params.id)
+	req.db.collection('user').remove({"_id": o_id}, function (err, result) {
+		if (err) {
+			req.flash('error', err)
+			res.redirect('/users')
+		} else {
+			req.flash('Berhasil', 'Data berhasil dihapus')
+			res.redirect('/tampil')
+		}
+	})
 })
 
 module.exports = app;
